13d95e12cfe28318216b7a2e04059794b26b628f
tylerboyd <36950595+tylerboyd@users.noreply.github.com>
2019-07-01 16:37:25 +1200

    Added Login Page and Blood Sugar Setup Page

--- app/src/main/java/com/example/sixthsenseapp/LoginActivity.java
+++ app/src/main/java/com/example/sixthsenseapp/LoginActivity.java
@@ -1,13 +1,56 @@
 package com.example.sixthsenseapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ImageView backgroundImage;
+    private EditText email;
+    private EditText password;
+    private Button loginButton;
+    private TextView errorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        backgroundImage = findViewById(R.id.backgroundImage);
+        email = (EditText)findViewById(R.id.emailField);
+        password = (EditText)findViewById(R.id.passwordField);
+        loginButton = (Button)findViewById(R.id.loginButton);
+        errorMessage = (TextView)findViewById(R.id.errorMessage);
+
+        int imageResource = getResources().getIdentifier("@drawable/loginbackground", null, this.getPackageName());
+        backgroundImage.setImageResource(imageResource);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                validateData(email.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+    //NEEDS TO GET EMAIL AND PASSWORD DATA FROM DATABASE
+    private void validateData(String userEmail, String userPassword){
+        if((userEmail.equals("test")) && (userPassword.equals("test"))){
+            //Intent intent =  new Intent(LoginActivity.this, DashboardActivity.class);
+            //startActivity(intent);
+            Log.i("LoginSuccess", "Correct Email and Password!");
+            errorMessage.setText("");
+        }
+        else{
+            Log.i("LoginFailure", "Incorrect Email or Password!");
+            errorMessage.setText("Incorrect email or password!");
+        }
     }
 }
--- app/src/main/java/com/example/sixthsenseapp/SetupUserType.java
+++ app/src/main/java/com/example/sixthsenseapp/SetupUserType.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class SetupUserType extends AppCompatActivity {
@@ -21,13 +22,13 @@ public class SetupUserType extends AppCompatActivity {
         setContentView(R.layout.activity_setup_user_type);
 
         backgroundImage = findViewById(R.id.backgroundImage);
-        int imageResource = getResources().getIdentifier("@drawable/usertype", null, this.getPackageName());
-        backgroundImage.setImageResource(imageResource);
-
-        //mineButton = findViewById(R.id.setupButton);
-        //someoneElseButton = findViewById(R.id.setupButton);
+        mineButton = findViewById(R.id.mineButton);
+        someoneElseButton = findViewById(R.id.someoneElseButton);
         nextButton = findViewById(R.id.nextButton);
         backButton = findViewById(R.id.backButton);
+
+        int imageResource = getResources().getIdentifier("@drawable/usertype", null, this.getPackageName());
+        backgroundImage.setImageResource(imageResource);
 
         nextButton.setOnClickListener(new View.OnClickListener(){
             @Override
