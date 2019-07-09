3511bcfe6f1efa32a5cc362883f36dd36a6cca1e
tylerboyd <36950595+tylerboyd@users.noreply.github.com>
2019-07-09 14:52:14 +1200

    Implemented firebase

--- app/src/main/java/com/example/sixthsenseapp/SetupPrimaryTreatment.java
+++ app/src/main/java/com/example/sixthsenseapp/SetupPrimaryTreatment.java
@@ -1,16 +1,14 @@
 package com.example.sixthsenseapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class SetupPrimaryTreatment extends AppCompatActivity {
