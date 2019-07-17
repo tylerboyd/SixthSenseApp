a9b7208366c38e207948df15deaba644df4cd767
AaronZhenhaoZhong <353192072@qq.com>
2019-07-17 12:57:53 +1200

    countdown timmer, restTestBlood

--- app/src/main/java/com/example/sixthsenseapp/RetestBloodActivity.java
+++ app/src/main/java/com/example/sixthsenseapp/RetestBloodActivity.java
@@ -1,33 +1,71 @@
 package com.example.sixthsenseapp;
 
-import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class RetestBloodActivity extends AppCompatActivity {
     private static SeekBar seekbar;
-
+    EditText editText;
+    ImageButton tickButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.retestblood_activity);
 
-        seekbar=findViewById(R.id.seekBar);
+        seekbar=findViewById(R.id.seekBar2);
+        editText=findViewById(R.id.editText);
+        tickButton=findViewById(R.id.tickbutton);
+        final ConstraintLayout constraintLayout=findViewById(R.id.Layout);
 
+        constraintLayout.setBackgroundColor(Color.LTGRAY);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() == 0) {
+                    tickButton.setEnabled(false);
+                } else {
+                    tickButton.setEnabled(true);
+
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = s.toString();
+                if(!TextUtils.isEmpty(text)){
+                    seekbar.setProgress(Integer.parseInt(text));
+                }
+            }
+        });
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progressChangedValue = 5;
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progressChangedValue =progress;
+                if (progress >= 10){
+                    constraintLayout.setBackgroundColor(Color.RED);
+                }
+                else if (progress<=4){
+                    constraintLayout.setBackgroundColor(Color.RED);
+                }
+                else if  (progress>4 && progress<10)
+                {
+                    constraintLayout.setBackgroundColor(Color.LTGRAY);
+                }
 
-                Toast.makeText(RetestBloodActivity.this, "Seek bar progress is :" + progressChangedValue,
-                        Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
@@ -40,6 +78,7 @@ public class RetestBloodActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
 
