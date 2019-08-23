50672f62e5665d8947e2c9fd85f6b7241aa0f2f4
Tomas <kyg8185@autuni.ac.nz>
2019-08-24 11:53:53 +1200

    Settings Merged
    attempt 2

--- app/src/main/java/com/example/sixthsenseapp/Dashboard.java
+++ app/src/main/java/com/example/sixthsenseapp/Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.sixthsenseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,17 +9,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.sixthsenseapp.Settings.Tabs;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Dashboard extends AppCompatActivity {
 
     private Button logOutButton;
+    private Button settingsButton;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         logOutButton = findViewById(R.id.logoutButton);
 
@@ -35,5 +46,20 @@ public class Dashboard extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        settingsButton = (Button) findViewById(R.id.buttonsetting);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Dashboard.this, Tabs.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/sixthsenseapp/Settings/tab1.java
@@ -0,0 +1,87 @@
+package com.example.sixthsenseapp.Settings;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.example.sixthsenseapp.R;
+
+public class tab1 extends Fragment {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
+    private Switch switch1;
+    private Switch switch2;
+
+    private int timer = 15;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab1, container, false);
+
+        button1 = (Button) v.findViewById(R.id.buttonMinus);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer = timer - 5;
+                Toast toast = Toast.makeText(v.getContext(), "Timer Minus 5 min: " + timer, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        button2 = (Button) v.findViewById(R.id.buttonPlus);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer = timer + 5;
+                Toast toast = Toast.makeText(v.getContext(), "Timer Plus 5 min: " + timer, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        button3 = (Button) v.findViewById(R.id.buttonInfo);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(v.getContext(), "Further Information Available In The Future", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        Switch switch1 = (Switch) v.findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast toast = Toast.makeText(buttonView.getContext(), "ON", Toast.LENGTH_LONG);
+                } else {
+                    Toast toast = Toast.makeText(buttonView.getContext(), "OFF", Toast.LENGTH_LONG);
+                }
+            }
+        });
+
+        Switch switch2 = (Switch) v.findViewById(R.id.switch2);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast toast = Toast.makeText(buttonView.getContext(), "ON", Toast.LENGTH_LONG);
+                } else {
+                    Toast toast = Toast.makeText(buttonView.getContext(), "OFF", Toast.LENGTH_LONG);
+                }
+            }
+        });
+
+        return v;
+    }
+
+}
