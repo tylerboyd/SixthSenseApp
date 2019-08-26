9796d5000e4b58a4fedb7fbdd036da96c6f37443
Tomas <kyg8185@autuni.ac.nz>
2019-08-26 16:34:28 +1200

    Trying to Fix Git

--- app/src/main/java/com/example/sixthsenseapp/MainActivity.java
+++ app/src/main/java/com/example/sixthsenseapp/MainActivity.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.sixthsenseapp;
 
 import android.content.Intent;
@@ -43,3 +44,68 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+=======
+package com.example.sixthsenseapp;
+
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.sixthsenseapp.Setup.SetupAddCaregiver;
+import com.example.sixthsenseapp.Setup.SetupUserType;
+import com.example.sixthsenseapp.Setup.SetupVerification;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView backgroundImage;
+    private Button loginButton;
+    private Button setupButton;
+    private Button verificationButton;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        backgroundImage = findViewById(R.id.backgroundImage);
+        loginButton = findViewById(R.id.loginButton);
+        setupButton = findViewById(R.id.setupButton);
+
+
+        int imageResource = getResources().getIdentifier("@drawable/startscreen", null, this.getPackageName());
+        backgroundImage.setImageResource(imageResource);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        setupButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, SetupUserType.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+        //Back Button disabled on Main screen
+        Toast.makeText(getApplicationContext(), "Cannot go back, you are not logged in.", Toast.LENGTH_LONG).show();
+    }
+}
+>>>>>>> Stashed changes
